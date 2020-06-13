db8a6721348d715009408aba755046007be9c269
D0HaTeJIJI0 <trav_git@mail.ru>
2020-06-13 15:34:29 +0300

    feat: add pagination

--- front-end/src/js/index.js
+++ front-end/src/js/index.js
@@ -1,6 +1,11 @@
-var currentEmployees = [];
+var state = {
+  'employees': [],
+  'page': 1,
+  'pageSize': 3,
+  'totalPageNumber': 0
+}
 
-fetchEmployees();
+fetchEmployees(state.page, state.pageSize);
 
 let ths = document.getElementsByTagName('th');
 
@@ -13,40 +18,42 @@ for (let th of ths) {
 
     if (order == 'desc') {
       th.dataset["order"] = 'asc';
-      currentEmployees = currentEmployees.sort((a, b) => a[column] == b[column] ? 0 :
+      state.employees = state.employees.sort((a, b) => a[column] == b[column] ? 0 :
                                                           a[column] < b[column] ? 1 :
                                                                                   -1);
       text += "&#9650";
-      buildTable(currentEmployees)
+      buildTable();
     } else {
       th.dataset["order"] = 'desc';
-      currentEmployees = currentEmployees.sort((a, b) => a[column] == b[column] ? 0 :
+      state.employees = state.employees.sort((a, b) => a[column] == b[column] ? 0 :
                                                           a[column] > b[column] ? 1 :
                                                                                   -1);
       text += "&#9660";
-      buildTable(currentEmployees)
+      buildTable();
     }
     th.innerHTML = text;
   }
 }
 
 async function fetchEmployees() {
-  const response = await fetch('http://localhost:8081/referenceList/employee/all');
+  const response = await fetch(`http://localhost:8081/referenceList/employee/all?page=${state.page-1}&size=${state.pageSize}`);
   if (response.ok) {
     const pageEmployeeJson = await response.json();
 
     if (pageEmployeeJson) {
-      currentEmployees = pageEmployeeJson.employeeDtos;
+      state.employees = pageEmployeeJson.employeeDtos;
+      state.totalPageNumber = pageEmployeeJson.totalPageNumber;
 
-      buildTable(currentEmployees)
+      buildTable();
+      buildPagination();
     }
   }
 }
 
-function buildTable(employees) {
+function buildTable() {
   let employeeRows = "";
 
-  employees.forEach(employee => {
+  state.employees.forEach(employee => {
     employeeRows += "<tr>";
     employeeRows += "<td>" + employee.id + "</td>";
     employeeRows += "<td>" + employee.name + "</td>";
@@ -57,4 +64,24 @@ function buildTable(employees) {
   });
   
   document.getElementById("employees").innerHTML = employeeRows;
+}
+
+function buildPagination() {
+  let wrapper = document.getElementById('pagination-wrapper');
+  let buttons = "";
+
+  for (let page = 1; page <= state.totalPageNumber; page++) {
+    buttons += `<button value=${page} class="page btn btn-sm btn-info">${page}</button>`;
+  }
+
+  wrapper.innerHTML = buttons;
+
+  let pages = document.getElementsByClassName("page");
+  for (let page of pages) {
+    page.onclick = () => {
+      state.page = page.value;
+      fetchEmployees();
+      buildTable();
+    }
+  }
 }
